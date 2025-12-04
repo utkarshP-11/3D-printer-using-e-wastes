861647a4ac044812613d42088bd5301885b5bea3
Utkarsh Pandey <utkarsh.p.work@gmail.com>
2025-12-04 23:09:51 +0530

    marlin updated

--- Marlin-2.1.2.5/Marlin/Configuration.h
+++ Marlin-2.1.2.5/Marlin/Configuration.h
@@ -88,7 +88,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+#define MOTHERBOARD BOARD_ESPRESSIF_ESP32
 #endif
 
 // @section serial
@@ -101,7 +101,7 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT 0
+#define SERIAL_PORT -1
 
 /**
  * Serial Port Baud Rate
@@ -114,7 +114,7 @@
  *
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
-#define BAUDRATE 250000
+#define BAUDRATE 115200
 
 //#define BAUD_RATE_GCODE     // Enable G-code M575 to set the baud rate
 
--- Marlin-2.1.2.5/Marlin/src/pins/esp32/pins_ESP32.h
+++ Marlin-2.1.2.5/Marlin/src/pins/esp32/pins_ESP32.h
@@ -1,88 +1,60 @@
-/**
- * Marlin 3D Printer Firmware
- * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
- *
- * Based on Sprinter and grbl.
- * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- */
 #pragma once
-
-/**
- * Espressif ESP32 (Tensilica Xtensa LX6) pin assignments
- */
-
-#include "env_validate.h"
 
 #define BOARD_INFO_NAME "Espressif ESP32"
 
 //
-// I2S (steppers & other output-only pins)
+// 1. DISABLE THE VIRTUAL I2S PINS (You don't need them)
 //
-#ifndef I2S_STEPPER_STREAM
-  #define I2S_STEPPER_STREAM
-#endif
-#if ENABLED(I2S_STEPPER_STREAM)
-  #define I2S_WS                              25
-  #define I2S_BCK                             26
-  #define I2S_DATA                            27
-#endif
+// (We just won't include the code for them)
 
 //
-// Limit Switches
+// 2. DEFINE YOUR REAL MOTOR PINS (Update these to YOUR wiring!)
 //
-#define X_MIN_PIN                             34
-#define Y_MIN_PIN                             35
-#define Z_MIN_PIN                             15
+// X Axis
+#define X_STEP_PIN      26  // Change to where your X Step wire is
+#define X_DIR_PIN       25  // Change to where your X Dir wire is
+#define X_ENABLE_PIN    33  // Change to where your X Enable wire is
+
+// Y Axis
+#define Y_STEP_PIN      27
+#define Y_DIR_PIN       26
+#define Y_ENABLE_PIN    25
+
+// Z Axis
+#define Z_STEP_PIN      14
+#define Z_DIR_PIN       12
+#define Z_ENABLE_PIN    13
+
+// Extruder (E0)
+#define E0_STEP_PIN     16
+#define E0_DIR_PIN      17
+#define E0_ENABLE_PIN   5
 
 //
-// Steppers
+// 3. LIMIT SWITCHES (Inputs)
 //
-#define X_STEP_PIN                           128
-#define X_DIR_PIN                            129
-#define X_ENABLE_PIN                         130
-//#define X_CS_PIN                             0
-
-#define Y_STEP_PIN                           131
-#define Y_DIR_PIN                            132
-#define Y_ENABLE_PIN                         133
-//#define Y_CS_PIN                            13
-
-#define Z_STEP_PIN                           134
-#define Z_DIR_PIN                            135
-#define Z_ENABLE_PIN                         136
-//#define Z_CS_PIN                             5  // SS_PIN
-
-#define E0_STEP_PIN                          137
-#define E0_DIR_PIN                           138
-#define E0_ENABLE_PIN                        139
-//#define E0_CS_PIN                           21
+#define X_MIN_PIN       34
+#define Y_MIN_PIN       35
+#define Z_MIN_PIN       39
 
 //
-// Temperature Sensors
+// 4. TEMPERATURE & HEATERS
 //
-#define TEMP_0_PIN                            36  // Analog Input
-#define TEMP_BED_PIN                          39  // Analog Input
+#define TEMP_0_PIN      36  // Hotend Thermistor
+#define HEATER_0_PIN    2   // Hotend Mosfet/Relay
+
+#define TEMP_BED_PIN    39  // Bed Thermistor (If you have one)
+#define HEATER_BED_PIN  4   // Bed Mosfet (If you have one)
 
 //
-// Heaters / Fans
+// 5. FAN
 //
-#define HEATER_0_PIN                           2
-#define FAN0_PIN                              13
-#define HEATER_BED_PIN                         4
+#define FAN0_PIN        15  // Part cooling fan
 
-// SPI
-#define SDSS                                   5
+//
+// 6. SPI for SD CARD (Standard VSPI)
+//
+#define SDSS            5
+#define MOSI_PIN        23
+#define MISO_PIN        19
+#define SCK_PIN         18
